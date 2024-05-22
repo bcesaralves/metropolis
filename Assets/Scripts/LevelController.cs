44f62e95700748dbc8326f2dd6623fd1526d2cb5
Bruno Cesar Alves <bcesaralves@gmail.com>
2024-05-22 19:48:21 +0100

    Make LevelController singleton

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -2,9 +2,22 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+    public static LevelController instance = null;
     public CardArranger arranger;
     // Example variable to hold level parameters received from GameController
     private LevelParameters levelParams;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void Start()
     {
