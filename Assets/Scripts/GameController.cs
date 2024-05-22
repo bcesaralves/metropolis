bb0241c6bc3547016a77e461864fc02f42fec119
Bruno Cesar Alves <bcesaralves@gmail.com>
2024-05-22 21:26:58 +0100

    Implement a save/load system to persist progress between game restarts

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,7 +10,9 @@ public class GameController : MonoBehaviour
     public enum GameState { MainMenu, Playing, GameOver };
     public GameState currentState = GameState.MainMenu;
     public int score = 0;
-    private LevelParameters levelParameters;
+    public List<LevelParameters> levels;
+    private string saveFilePath;
+    private GameProgress progress;
 
     void Awake()
     {
@@ -24,19 +28,47 @@ public class GameController : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    void Update()
+    void Start()
     {
+        saveFilePath = Path.Combine(Application.persistentDataPath, "savegame.json");
+        LoadGame();
+    }
 
+    public void LoadGame()
+    {
+        if (File.Exists(saveFilePath))
+        {
+            string json = File.ReadAllText(saveFilePath);
+            progress = JsonUtility.FromJson<GameProgress>(json);
+            Debug.Log("Game loaded successfully.");
+        }
+        else
+        {
+            Debug.Log("Initializing fresh game.");
+            InitializeFreshGame();
+        }
+    }
+
+    public void SaveGame()
+    {
+        string json = JsonUtility.ToJson(progress, true);
+        File.WriteAllText(saveFilePath, json);
+        Debug.Log("Game saved successfully.");
+    }
+
+    void InitializeFreshGame()
+    {
+        progress = new GameProgress
+        {
+            level = 1,
+            score = 0
+        };
     }
 
     public void StartLevel()
     {
         currentState = GameState.Playing;
         score = 0;
-        levelParameters = new LevelParameters();
-        levelParameters.horizontalNumberOfCards = 2;
-        levelParameters.verticalNumberOfCards = 2;
-        levelParameters.type = arrangeType;
         SceneManager.LoadScene("Game");
     }
 
@@ -54,6 +86,11 @@ public class GameController : MonoBehaviour
 
     public void LevelFinished()
     {
+        if(progress.level < levels.Count)
+        {
+            progress.level++;
+        }
+        SaveGame();
         currentState = GameState.MainMenu;
         SceneManager.LoadScene("MainMenu");
     }
@@ -68,6 +105,6 @@ public class GameController : MonoBehaviour
 
     public LevelParameters GetLevelParameters()
     {
-        return levelParameters;
+        return levels[progress.level - 1];
     }
 }
---
+++ Assets/Scripts/GameProgress.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public class GameProgress
+{
+    public int level;
+    public int score;
+}
